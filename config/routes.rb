0163fc7c563ebab43a9cabd4e2bbdc7b890d07e0
Cosmel Villalobos <cosmelvg@gmail.com>
2022-07-08 16:03:54 -0400

    Update routes
    - Add GET route to search person for the API
    - Add new resources for companies controller including index new and create
    - Add GET route to search companies for the API
    - Add POST route to create multiply companies

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@
 
 Rails.application.routes.draw do
   resources :people, only: %i[index new create]
+  get 'search_person', to: 'people#search', as: 'search'
+  resources :companies, only: %i[index new create]
+  get 'serach_company', to: 'companies#search_company', as: 'searchcompany'
+  post 'create_multiple_companies', to: 'companies#create_multiple_companies', as: 'createmultiplecompanies'
   root to: 'people#index'
 end
