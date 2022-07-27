b6cfc1bd8f1bb9e2e5d8ca1dbc56b46f5f6bff50
Josh McGinnis <joshua@mcginnis.io>
2022-07-27 15:04:05 -0700

    Fix view not rendering phone_number and implement view spec.

--- spec/views/people/index.html.slim_spec.rb
+++ spec/views/people/index.html.slim_spec.rb
@@ -1,5 +1,27 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe "people/index.html.slim" do
-  it "Displays the users"
+RSpec.describe 'people/index' do
+  let(:company) { instance_double(Company, name: 'test inc') }
+
+  let(:person) do
+    instance_double(Person,
+                    id: 1,
+                    name: 'test',
+                    phone_number: '999-999-9999',
+                    email: 'test@example.com',
+                    company: company
+                   )
+  end
+
+  it 'displays a table of users' do
+    assign(:people, [person])
+
+    render
+
+    expect(rendered).to match(/1/)
+    expect(rendered).to match(/test/)
+    expect(rendered).to match(/999-999-9999/)
+    expect(rendered).to match(/test@example.com/)
+    expect(rendered).to match(/test inc/)
+  end
 end
